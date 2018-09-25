a39246fecd287008e8dcad813d311577840a11a8
Akshay Nair <phenax5@gmail.com>
2018-09-25 16:22:05 +0530

    Adds pattern matching test cases for enum

--- test/index.test.js
+++ test/index.test.js
@@ -19,5 +19,48 @@ describe('EnumType', () => {
             instance.Action2();
         });
     });
+
+    describe('matching', () => {
+        it('should match the correct function and call it', () => {
+            const Type = EnumType([ 'Add', 'Delete' ]);
+
+            const action = Type.Add();
+
+            const onAdd = jest.fn(() => 'Adding');
+            const result = Type.match(action, {
+                Add: onAdd,
+                Delete: () => 'Deleting',
+                _: () => 'Default',
+            });
+
+            expect(result).toBe('Adding');
+            expect(onAdd).toHaveBeenCalledTimes(1);
+        });
+
+        it('should call the default function when the action is not specified', () => {
+            const Type = EnumType([ 'Add', 'Delete' ]);
+
+            const action = Type.Delete();
+
+            const handleDefault = jest.fn(() => 'Default');
+            const result = Type.match(action, {
+                Add: () => 'Adding',
+                _: handleDefault,
+            });
+
+            expect(result).toBe('Default');
+            expect(handleDefault).toHaveBeenCalledTimes(1);
+        });
+
+        it('should call the default function when the action is not specified', () => {
+            const Type = EnumType([ 'Add', 'Delete' ]);
+
+            const action = Type.Delete();
+
+            expect(() => Type.match(action, {
+                Add: () => 'Adding',
+            })).toThrowError();
+        });
+    });
 });
 
