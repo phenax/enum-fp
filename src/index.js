d861c076aed287c8a8ddb978f548f64a0c9c44c9
Akshay Nair <phenax5@gmail.com>
2018-10-06 19:23:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,28 +2,3 @@
 import EnumType from './EnumType';
 
 export default EnumType;
-
-/*
-
-TODO: Allow passing object instead of array
-TODO: Add specify predicate to validate value
-
-EnumType({
-    Add: message => typeof message === 'string',
-});
-
-OR
-
-EnumType({
-    Add: [ 'string' ],
-});
-
-OR
-
-EnumType({
-    Add: {
-        Todo: todo => todo && typeof todo.name === 'string',
-        NewTodo: name => typeof name === 'string',
-    },
-})
-*/
