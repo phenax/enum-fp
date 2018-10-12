80d09e4d4ffc434a58a5e5d4067abe3fbec68b86
Akshay Nair <phenax5@gmail.com>
2018-10-12 21:59:32 +0530

    Adds match method test cases for subtype

--- test/EnumTag.test.js
+++ test/EnumTag.test.js
@@ -46,4 +46,83 @@ describe('EnumTag', () => {
             expect(Tag().is(Tag2())).not.toBeTruthy();
         });
     });
+
+    describe('#match', () => {
+        
+        it('should match the correct function and call it', () => {
+            const Type = EnumType([ 'Add', 'Delete' ]);
+
+            const action = Type.Add();
+
+            const onAdd = jest.fn(() => 'Adding');
+            const result = action.match({
+                Add: onAdd,
+                Delete: () => 'Deleting',
+                _: () => 'Default',
+            });
+
+            expect(result).toBe('Adding');
+            expect(onAdd).toHaveBeenCalledTimes(1);
+        });
+
+        it('should call the default function when the action is not specified', () => {
+            const Type = EnumType([ 'Add', 'Delete' ]);
+
+            const action = Type.Delete();
+
+            const handleDefault = jest.fn(() => 'Default');
+            const result = action.match({
+                Add: () => 'Adding',
+                _: handleDefault,
+            });
+
+            expect(result).toBe('Default');
+            expect(handleDefault).toHaveBeenCalledTimes(1);
+        });
+
+        it('should call the default function when the action is not specified', () => {
+            const Type = EnumType([ 'Add', 'Delete' ]);
+
+            const action = Type.Delete();
+
+            expect(() => action.match({
+                Add: () => 'Adding',
+            })).toThrowError();
+        });
+
+        it('should match the correct function and call it with the constructor arguements', () => {
+            const Type = EnumType([ 'Add', 'Delete' ]);
+
+            const action = Type.Add('Hello', 'World');
+
+            const onAdd = jest.fn((str1, str2) => `Adding - ${str1} ${str2}`);
+            const result = action.match({
+                Add: onAdd,
+                Delete: () => 'Deleting',
+                _: () => 'Default',
+            });
+
+            expect(result).toBe('Adding - Hello World');
+            expect(onAdd).toHaveBeenCalledTimes(1);
+        });
+
+        it('should match the correct function and call it with the constructor arguements', () => {
+            const Type = EnumType({
+                Add: [ 'id', 'text' ],
+                Delete: [ 'id' ],
+            });
+
+            const pattern = {
+                Add: jest.fn((id, name) => `Adding - [${id}] ${name}`),
+                Delete: jest.fn(id => `Deleting - [${id}]`),
+            };
+            const resultOnAdd = Type.Add(5, 'Hello World').match(pattern);
+            const resultOnDelete = Type.Delete(5).match(pattern);
+
+            expect(resultOnAdd).toBe('Adding - [5] Hello World');
+            expect(pattern.Add).toHaveBeenCalledTimes(1);
+            expect(resultOnDelete).toBe('Deleting - [5]');
+            expect(pattern.Delete).toHaveBeenCalledTimes(1);
+        });
+    });
 });
