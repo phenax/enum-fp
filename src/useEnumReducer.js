861cb4fc7c342a3b098d08e1689f821cbf5f5394
Akshay Nair <phenax5@gmail.com>
2018-11-04 16:12:29 +0530

    Adds useReducer tests

--- src/useEnumReducer.js
+++ src/useEnumReducer.js
@@ -3,9 +3,10 @@ import { useReducer } from 'react';
 
 // type Config = { useReducer :: React.Hook };
 
-// useReducer :: (Reducer, State) -> [ State, EnumTagType -> () ]
-export default createHook({ useReducer });
 
 // useReducer :: Config -> (Reducer, State) -> [ State, EnumTagType -> () ]
 export const createHook = ({ useReducer }) =>
     (reducer, initialState) => useReducer((state, action) => reducer(action)(state), initialState);
+
+// useReducer :: (Reducer, State) -> [ State, EnumTagType -> () ]
+export default createHook({ useReducer });
--- test/useEnumReducer.test.js
+++ test/useEnumReducer.test.js
@@ -1,36 +1,59 @@
 // import React from 'react';
 
-// import EnumType from '../src/EnumType';
-// import reducerComponent from '../src/reducerComponent';
+import Enum from '../src/Enum';
+import { createHook } from '../src/useEnumReducer';
 
-// const TestType = EnumType([ 'Action1', 'Action2', 'Action3' ]);
+// mockReducerHook :: (a -> a) -> a -> [a, Action -> (), () -> a]
+const mockReducerHook = (reducer, _state) => {
+    let state = _state;
+    const dispatch = action => state = reducer(state, action);
+    return [ state, dispatch, () => state ];
+};
 
-// const reducer = TestType.caseOf({
-//     Action1: () => ({ action: 'Action1' }),
-//     Action2: () => ({ action: 'Action2' }),
-//     Action3: () => ({ action: 'Action3' }),
-// });
+const useEnumReducer = createHook({ useReducer: mockReducerHook });
 
-// const state = {
-//     action: '',
-// };
-
-// class Comp extends React.Component {
-//     componentDidMount() {
-//         this.props.dispatch(TestType.Action1());
-//     }
-//     render = () => null
-// }
+const Actions = Enum([ 'A1', 'A2', 'A3', 'A4', 'A5' ]);
 
 describe('React', () => {
 
-    describe('reducerComponent', () => {
+    describe('useEnumReducer', () => {
 
-        // const EnhancedComp = reducerComponent({ state, reducer })(Comp);
+        it('should update state with dispatches', () => {
 
-        it('should dispatch Action1', () => {
+            const reducer = Actions.caseOf({
+                A1: by => state => state + by,
+                A2: by => state => state - by,
+                _: () => state => state,
+            });
+
+            const [ _, dispatch, getState ] = useEnumReducer(reducer, 0);
             
-            
+            expect(getState()).toBe(0);
+
+            dispatch(Actions.A1(5));
+            dispatch(Actions.A1(7));
+            dispatch(Actions.A2(2));
+            dispatch(Actions.A3(5));
+            dispatch(Actions.A2(1));
+
+            expect(getState()).toBe(9);
+        });
+
+        it('should throw an error for invalid action', () => {
+
+            const reducer = Actions.caseOf({
+                _: () => state => state,
+            });
+
+            const [ _, dispatch ] = useEnumReducer(reducer, 0);
+
+            expect(() => dispatch(Actions.InvalidAction())).toThrowError();
+            expect(() => dispatch(null)).toThrowError();
+            expect(() => dispatch(undefined)).toThrowError();
+            expect(() => dispatch({})).toThrowError();
+            expect(() => dispatch(1)).toThrowError();
+            expect(() => dispatch('1')).toThrowError();
+            expect(() => dispatch()).toThrowError();
         });
     });
 });
