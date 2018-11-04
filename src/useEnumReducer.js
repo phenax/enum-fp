1e4ba64ec8cc38fb11fb209cf2c900ab7606926e
Akshay Nair <phenax5@gmail.com>
2018-11-04 14:23:14 +0530

    Adds createHook helper for testability

--- src/useEnumReducer.js
+++ src/useEnumReducer.js
@@ -2,4 +2,8 @@
 import { useReducer } from 'react';
 
 // useReducer :: (Reducer, State) -> [ State, EnumTagType -> () ]
-export default (reducer, initialState) => useReducer((state, action) => reducer(action)(state), initialState);
+export default createHook(useReducer);
+
+// useReducer :: (Reducer, State) -> [ State, EnumTagType -> () ]
+export const createHook = ({ useReducer }) =>
+    (reducer, initialState) => useReducer((state, action) => reducer(action)(state), initialState);
