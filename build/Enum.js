c3ce262f1b4223c9be7128397d4690aaa303e9bf
Akshay Nair <phenax5@gmail.com>
2018-11-11 19:18:39 +0530

    Build minified furhter

--- build/Enum.js
+++ build/Enum.js
@@ -1,9 +1,1 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:true});exports.default=exports.createConstructor=void 0;var _utils=require("./utils");var _Type=require("./Type");// type TypeConstructor = ...a -> EnumTagType
-// createConstructor :: (Enum, ConstructorDescription) -> TypeConstructor
-var createConstructor=function createConstructor(Type,_ref){var name=_ref.name,props=_ref.props;return function(){for(var _len=arguments.length,args=new Array(_len),_key=0;_key<_len;_key++){args[_key]=arguments[_key]}if(props?!(0,_Type.validateArgs)(props,args):false)throw new TypeError("Invalid number of args passed to constructor ".concat(name));var self={// args :: Array *
-args:args,// name :: String
-name:name,// props :: ?Array String
-props:props,// is :: String | EnumTagType | ConstructorDescription ~> Boolean
-is:function is(otherType){return[otherType,otherType.name].indexOf(name)!==-1},// match :: Object (* -> b) ~> b
-match:function match(pattern){return Type.match(self,pattern)}};return self}};// Enum :: Array String | Object * -> Enum
-exports.createConstructor=createConstructor;var _default=(0,_utils.createEnumFactory)({createConstructor:createConstructor});exports.default=_default;
+"use strict";Object.defineProperty(exports,"__esModule",{value:true});exports.default=exports.createConstructor=void 0;var _utils=require("./utils");var _Type=require("./Type");var createConstructor=function createConstructor(Type,_ref){var name=_ref.name,props=_ref.props;return function(){for(var _len=arguments.length,args=new Array(_len),_key=0;_key<_len;_key++){args[_key]=arguments[_key]}if(props?!(0,_Type.validateArgs)(props,args):false)throw new TypeError("Invalid number of args passed to constructor ".concat(name));var self={args:args,name:name,props:props,is:function is(otherType){return[otherType,otherType.name].indexOf(name)!==-1},match:function match(pattern){return Type.match(self,pattern)}};return self}};exports.createConstructor=createConstructor;var _default=(0,_utils.createEnumFactory)({createConstructor:createConstructor});exports.default=_default;
